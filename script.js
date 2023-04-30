0ecf9e66af8c575227a2f3b09e8049e7513a604c
shahrozali <0024shahroz@gmail.com>
2023-04-30 10:47:12 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+teacherNames = ["Numan", "Uzair", "ALi"]
+
+// teacherNames.push("shahroz")
+// teacherNames.unshift("shahroz")
+// teacherNames.pop()
+// teacherNames.shift()
+
+// teacherNames.splice(1,1,"shah")
+
+// teacherNames.splice(2,1)
+
+teacherNames.splice(2,1,"farhan","Manzoor")
+
+teacherNames.splice(3,0,"shahroz")
+
+ console.log(teacherNames)
+
+// write a js program, that take input username from form input, and save into start of array,
+// and perform search as well if user search the name
